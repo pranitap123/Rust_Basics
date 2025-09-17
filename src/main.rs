afb178fc14d118bc71531828f0fd80c62df8591c
pranitap123 <pranitapanchal339@gmail.com>
2025-09-17 13:26:41 +0530

    Updated changes

--- src/main.rs
+++ src/main.rs
@@ -99,7 +99,7 @@ fn main(){
 }
 */
 
-//using if in let statement
+/*using if in let statement
 //Because if is an expression, we can use it on the right side of a let statement to assign the outcome to a variable
 
 fn main(){
@@ -109,4 +109,86 @@ fn main(){
 
     println!("The number is: {number}");
 
+}*/
+
+/*---------Loops--------------- */
+/*fn main(){
+    let mut counter = 0;
+    let result = loop{
+        counter += 1;
+
+        if counter == 10{
+            break counter * 2;
+        }
+    };
+
+    println!("The result is: {result} ");
+}*/
+
+/*Loop Labels to Disambiguate Between Multiple Loops 
+
+fn main(){
+    let mut count = 0;
+    'counting_up : loop{
+         println!("count = {count} ");
+
+         let mut remaining = 10;
+
+         loop{
+            println!("remaining = {remaining}");
+            if remaining == 9{
+                break;
+            }
+            if count == 2{
+                break 'counting_up;
+            }
+            remaining -= 1;
+         }
+         count += 1;
+    }
+
+    println!("End count = {count}");
+} */
+
+/*-----------While loop-------------- 
+fn main() {
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+} */
+
+/*--------Looping Through a Collection with for------------- 
+fn main(){
+    let a = [10,20,30,40,50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("The value is: {} ", a[index]);
+        index += 1;
+    }
+} */
+
+//concise way to do this using for loop:
+
+/*fn main(){
+    let a = [10,20,30,40,50];
+
+    for elements in a {
+        println!("The value is: {elements}");
+    }
+}*/
+
+// to reverse the range:
+
+fn main(){
+    for number in (1..4).rev(){
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!!!!!");
 }
