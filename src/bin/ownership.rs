cac57dfd7500655949b5feeb2bad25a915ddd448
pranitap123 <pranitapanchal339@gmail.com>
2025-09-19 20:31:34 +0530

    File updated

--- src/bin/ownership.rs
+++ src/bin/ownership.rs
@@ -20,7 +20,7 @@ Therefore, any automatic copying can be assumed to be inexpensive in terms of ru
     println!("{}, {}", greet, greet_dup);
     //If we do want to deeply copy the heap data of the String, not just the stack data, we can use a common method called clone
     //When you see a call to clone, you know that some arbitrary code is being executed and that code may be expensive.
-}
+
 /*we said that when a variable goes out of scope, Rust automatically calls the drop function and cleans up the heap memory
  for that variable. But Figure 4-2 shows both data pointers pointing to the same location. 
  This is a problem: when s2 and s1 go out of scope, they will both try to free the same memory. This is known as a double free error and is one of the memory safety bugs we mentioned previously.
@@ -29,3 +29,14 @@ Therefore, any automatic copying can be assumed to be inexpensive in terms of ru
  /*To ensure memory safety, after the line let s2 = s1;, Rust considers s1 as no longer valid.
   Therefore, Rust doesn’t need to free anything when s1 goes out of scope. 
  Check out what happens when you try to use s1 after s2 is created; it won’t work: */
+
+ /*When you assign a new value to an existing variable, Rust immediately calls drop on the old value to free its memory. */
+
+ let mut s = String::from("hello");
+ s = String::from("ahoy");
+
+ println!("{s}, world!");
+
+
+ /*Here, the original "hello" string is dropped as soon as "ahoy" is assigned, since nothing refers to "hello" anymore. The variable s now points to the new heap data "ahoy". When printed, the output is "ahoy, world!". */
+}
